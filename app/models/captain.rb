e4028077fa27a40fcf74e64e8b88c9809b3fb057
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-14 15:56:46 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,19 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def catamaran_operators
+  def self.catamaran_operators
     includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
-  def sailors
+  def self.sailors
     includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
-  def talented_seafarers
+  def self.talented_seafarers
     where("id IN (?)", self.sailers.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
-  def non_sailors
+  def self.non_sailors
     where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
